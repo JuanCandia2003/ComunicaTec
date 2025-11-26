7e2b3f47b804ad59d0e00c40d5a33ede51712b42
JuanCandia2003 <candia.maldonado.juan.david@gmail.com>
2025-11-25 22:38:28 -0400

    comunicatec likes 2

---
+++ api/get_posts.php
@@ -0,0 +1,19 @@
+<?php
+header('Content-Type: application/json');
+require_once 'db.php';
+
+$sql = "SELECT * FROM posts ORDER BY created_at DESC";
+$result = $conn->query($sql);
+
+$posts = [];
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $posts[] = $row;
+    }
+}
+
+echo json_encode($posts);
+
+$conn->close();
+?>
